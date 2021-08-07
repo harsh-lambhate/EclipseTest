1a3d0f12714b035d3a3dd47d4e69795d4d3352b2
harsh-lambhate <harsh071998@gmail.com>
2021-08-07 11:00:18 +0530

    Changes occur

--- EclipseTest/src/Test/ABC.java
+++ EclipseTest/src/Test/ABC.java
@@ -2,9 +2,17 @@ package Test;
 
 public class ABC {
 	
+	ABC()
+	{
+		System.out.println("Hii this is my first project changes");
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Hii This is my first project setup");
+		ABC abc=new ABC();
+		
 	}
+	
+	
 
 }
