40214062dd551d9d2a310d92e7fa1a63f5971a84
harsh-lambhate <harsh071998@gmail.com>
2021-08-07 12:45:15 +0530

    Done commited

--- EclipseTest/src/Test/DEF.java
+++ EclipseTest/src/Test/DEF.java
@@ -5,12 +5,9 @@ public class DEF {
 	public static void main(String[] args) {
 		
 		System.out.println("Namaste");
-
 		System.out.println("Namaste");
 		System.out.println("Namaste");
-
 		System.out.println("Jai Shree Ganesh");
 		System.out.println("Jai Mata di");
-
 	}
 }
